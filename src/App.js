e232c54ce0d6122e6cb671c0aa2a372ae77d966b
Tony Kharioki <antolisah@gmail.com>
2021-05-26 02:27:50 +0300

    added recoil package and sample data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
+const inventory = {
+  a: { name: '🍗  Honey Glazed', price: 10 },
+  b: { name: '☕️  Java espresso', price: 15 },
+  c: { name: '🍧  Vanilla icecream', price: 9.5 },
+};
+
+const destinations = {
+  NBO: 50,
+  WST: 75,
+  THK: 90,
+};
+
 function App() {
   return (
-    <div className="App">
+    <div>
       App
     </div>
   );
