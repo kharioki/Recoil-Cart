9196d79d464d4a1b80a1a32074bdd429c23f0b23
Tony Kharioki <antolisah@gmail.com>
2021-05-26 03:16:53 +0300

    listing available items

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import {
 } from 'recoil';
 
 const inventory = {
-  a: { name: '🍗  Honey Glazed', price: 10 },
-  b: { name: '☕️  Java espresso', price: 15 },
-  c: { name: '🍧  Vanilla icecream', price: 9.5 },
+  a: { name: '🍗   Honey Glazed', price: 10 },
+  b: { name: '☕️   Java espresso', price: 15 },
+  c: { name: '🍧   Vanilla icecream', price: 9.5 },
 };
 
 const destinations = {
@@ -18,10 +18,56 @@ const destinations = {
   THK: 90,
 };
 
+const cartState = atom({
+  key: 'cartState',
+  default: {}
+});
+
 function App() {
   return (
+    <RecoilRoot>
+      <AvailableItems />
+      <Cart />
+    </RecoilRoot>
+  );
+}
+
+function AvailableItems() {
+  const [cart, setCart] = useRecoilState(cartState);
+
+  return(
     <div>
-      App
+      <h2>Available Items</h2>
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
+      <ul>
+        {
+          Object.entries(inventory).map(([id, { name, price }]) => (
+            <li key={id}>
+              {name} @ ${price.toFixed(2)}
+              <button onClick={() => {
+                setCart({ ...cart, [id]: (cart[id] || 0) + 1 });
+              }}>add</button>
+              {cart[id] && <button onClick={() => {
+                const copy = { ...cart };
+                if (copy[id] === 1) {
+                  delete copy[id];
+                  setCart(copy);
+                } else {
+                  setCart({ ...copy, [id]: (copy[id]) - 1 });
+                }
+              }}>rem</button>}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+function Cart() {
+  return(
+    <div>
+      <h2>Cart</h2>
     </div>
   );
 }
